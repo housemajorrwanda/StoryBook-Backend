f5613cecfa6181420b98b63e8c6270b81f5ad9a8
Ndizihiwe Benny Chrispin <ndizibaidu23@gmail.com>
2026-03-05 10:46:26 +0200

    feat: add file upload endpoint for tour files with size limits and URL response

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,15 @@ import {
   HttpExceptionFilter,
   AllExceptionsFilter,
 } from './common/filters/http-exception.filter';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // Increase body size limits so large multipart metadata doesn't get rejected
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
   app.useGlobalPipes(
     new ValidationPipe({
